67ea77bd63ce0bf2dc165da67cd529d867e6b513
chalseoko <chalseoko@gmail.com>
2015-12-21 22:37:02 -0500

    initial template

---
+++ tip calc/tip calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tip calc
+//
+//  Created by Chalse Okorom on 12/21/15.
+//  Copyright (c) 2015 chalse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
