b51e939f2ef78c1e601b0dd533b2c42f08d85e06
chalseoko <chalseoko@gmail.com>
2015-12-21 23:52:38 -0500

    save before reloading

--- tip calc/tip calc/ViewController.swift
+++ tip calc/tip calc/ViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var tip_calcLabel:
-        UILabel! 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
